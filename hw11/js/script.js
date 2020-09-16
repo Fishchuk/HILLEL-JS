55a68fdf6c00edc7dd91bf250e8ee3c7a457ad12
Tatyana Fishchuk <fishchuk.tatyana@gmail.com>
2020-09-16 17:13:28 +0300

    added hw 11

---
+++ hw11/js/script.js
@@ -0,0 +1,17 @@
+  
+function filterUpperCase(str) {
+    
+    return str
+        .split('')
+        .filter((i) => {
+            return i == i.toUpperCase() && i !== ' ' ? true : false;
+        })
+}
+
+    
+console.log(filterUpperCase('ПриВЕт'));
+		
+	
+
+ 
+
